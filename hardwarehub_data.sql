8daa5318e52d9dae051c0dbd1083b7163004c18b
fenix-jpg <60718098+fenix-jpg@users.noreply.github.com>
2023-05-15 19:54:52 -0700

    Add files via upload

---
+++ hardwarehub_data.sql
@@ -0,0 +1,2 @@
+INSERT INTO products (productID, pname, description, price, image)
+VALUES (1, 'CyberpowerPC Gamer Xtreme VR Gaming PC, Intel Core i7-12700F 2.1GHz, Geforce RTX 3060 12GB, 16GB DDR4, 1TB NVMe SSD, WiFi & Win 11 Home (GXiVR8040A12), Black', 'Gaming PC', 1269.99, 'PC1.jpg');
